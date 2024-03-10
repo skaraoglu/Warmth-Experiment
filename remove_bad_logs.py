5670ad9f7c266890c9c85f8bc73e908625e58eb9
Ethan Beaird <98125550+spectraldecomp@users.noreply.github.com>
2024-03-09 21:01:38 -0600

    Data and update log remover

--- remove_bad_logs.py
+++ remove_bad_logs.py
@@ -1,16 +1,9 @@
 import os
 import glob
 
-# Define the directory
-dir_path = '/logs/'
-
-# Get list of all .txt files in the directory
+dir_path = 'logs/'
 txt_files = glob.glob(dir_path + '*.txt')
-
-# Iterate over the list of filepaths & remove each file.
 for file_path in txt_files:
-    # Get the base name of the file (i.e., the file name without the directory info)
     base_name = os.path.basename(file_path)
-    # If the file name doesn't start with 'A', remove it
-    if not base_name.startswith('A'):
+    if not base_name.startswith('experiment_A'):
         os.remove(file_path)
